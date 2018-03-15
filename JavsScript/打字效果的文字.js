0dd24b965649e897e40b53a98d64ae7bb31f1de0
Wkan <616356908@qq.com>
2018-03-15 16:33:26 +0800

    2018年3月15日16:33:24

---
+++ JavsScript/打字效果的文字.js
@@ -0,0 +1,41 @@
+﻿var NewsTime = 2000;
+var TextTime = 50;
+var newsi = 0;
+var txti = 0;
+var txttimer;
+var newstimer;
+var newstitle = new Array();
+var newshref = new Array();
+newstitle[0] = "健康是身体的本钱";
+newshref[0] = "#";
+newstitle[0] = "关心身体，就是关心自己";
+newshref[0] = "#";
+newstitle[0] = "去西藏旅游了";
+newshref[0] = "#";
+newstitle[0] = "大雨倾盆，很大呀";
+newshref[0] = "#";
+
+function shownew() {
+    var endstr = "_";
+    hwnewstr = newstitle[newsi];
+    newslink = newshref[newsi];
+    if (txti == (hwnewstr,length -1)) {
+        endstr = "";
+    }
+    if (txti >= hwnewstr.length) {
+        clearInterval(txttimer);
+        clearInterval(newstimer);
+        newsi++;
+        if (newsi >= newstitle.length) {
+            newsi = 0;
+        }
+        newstimer = setInterval("shownew()", NewsTime);
+        txti = 0;
+        return;
+    }
+    clearInterval(txttimer);
+    document.getElementById("HotNews").href = newslink;
+    document.getElementById("HotNews").innerHTML = hwnewstr.substring(0, txti + 1) + endstr;
+    txttimer = setInterval("shownew()", TextTime);
+}
+shownew();
