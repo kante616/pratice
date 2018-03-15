c7bf5b195f4e8195a7e41d582f52ebbf4c93e7f1
Wkan <616356908@qq.com>
2018-03-15 14:10:42 +0800

    2018年3月15日14:10:37

---
+++ JavsScript/数字及时验证.js
@@ -0,0 +1,16 @@
+﻿function checkDecimal(element) {
+    var tmp = element.value.split(".");
+    if (!isNaN(element.value)) {
+        if (tmp.length!=2 || tmp[1].length != 2) {
+            document.myform.aaa.focus();
+            alert("输入金额请保留2位小数!");
+            document.getElementById('aaa').value = '0.00';
+            return false;
+        }
+    } else {
+        alert("输入金额必须是数字类型!");
+        document.getElementById('aaa').focus();
+        document.getElementById('aaa').value = '0.00';
+        return false;
+    }
+}
